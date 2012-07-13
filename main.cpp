3ac43750ae69d67735df37d443349afe69ed2d96
Jeroen van Wolffelaar <jeroen@wolffelaar.nl>
2012-07-13 16:02:12 +0200

    Fix reading in (variable width)

--- main.cpp
+++ main.cpp
@@ -15,22 +15,18 @@ void bailout( const char* w )
 
 int main(int argc, char** argv)
 {
-	//Map& map = *new Map;
-
 	std::vector<std::string> lines;
 	std::string line;
 	int width = -1;
 	while (std::getline(std::cin, line)) {
-		if (width < 0) width = line.size();
-		if ( width != line.size() )
-			bailout("Input error"); 
-
+		width = std::max<int>(width, line.size());
 		lines.push_back(line);
 	}
 
 	printf("%dx%d grid\n", width, lines.size());
 
 	Map& map = *new Map(width, lines.size());
+	memset(&map.at(0, 0), ' ', map.n*map.m);
 	for (int i=0;i<lines.size(); i++) 
 	{
 		char* mapline = &map.at(0, i);
--- state.h
+++ state.h
@@ -25,4 +25,5 @@ public:
 	}
 
 	void update();
+	bool validateMove(char move);
 };
