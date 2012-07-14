12ae85e2cb4c73c6b4f872114ca5b30a0fa3d37c
Jeroen van Wolffelaar <jeroen@wolffelaar.nl>
2012-07-14 20:25:38 +0200

    Bring enum byte values of mine content in line with the characters, easier on the GUI

---
+++ kyren/Tile.hpp
@@ -0,0 +1,41 @@
+#include <cstdint>
+
+enum class Tile : uint8_t {
+  Robot = 'R',
+  Wall = '#',
+  Rock = '*',
+  Lambda = '\\',
+  ClosedLift = 'L',
+  OpenLift = 'O',
+  Earth = '.',
+  TrampolineA = 'A',
+  TrampolineB = 'B',
+  TrampolineC = 'C',
+  TrampolineD = 'D',
+  TrampolineE = 'E',
+  TrampolineF = 'F',
+  TrampolineG = 'G',
+  TrampolineH = 'H',
+  TrampolineI = 'I',
+  Target1 = '1',
+  Target2 = '2',
+  Target3 = '3',
+  Target4 = '4',
+  Target5 = '5',
+  Target6 = '6',
+  Target7 = '7',
+  Target8 = '8',
+  Target9 = '9',
+  Empty = ' '
+};
+
+inline Tile tileFromChar(char c)
+{
+	return (Tile)c;
+}
+
+inline char charFromTile(Tile t)
+{
+	return (char)t;
+}
+
