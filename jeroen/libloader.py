ec174ffaa347e486d45e66a4b142eed777965058
OmnipotentEntity <OmnipotentEntity@gmail.com>
2012-07-15 09:24:51 -0400

    No more crash

--- jeroen/libloader.py
+++ jeroen/libloader.py
@@ -12,18 +12,9 @@ def LoadLibrary():
     libicfp.Init.argtypes = (c_char_p,)
     libicfp.Init.restype = data_t
     libicfp.GetData.argtypes = data_t, POINTER(c_int), POINTER(c_int)
-    libicfp.GetData.restype = POINTER(c_char)
+    libicfp.GetData.restype = POINTER(c_ulong)
     libicfp.DoMove.argtypes = data_t, c_char
     libicfp.DoMove.restype = c_int
-    if kyren:
-        libicfp.GetInfo.argtypes = data_t, POINTER(c_char), c_int, POINTER(c_int)
-        libicfp.GetInfo.restype = None
-        libicfp.GetSafeZone.argtypes = data_t,
-        libicfp.GetSafeZone.restype = POINTER(c_char)
-        libicfp.GoForIt.argtypes = data_t,
-        libicfp.GoForIt.restype = None
-        libicfp.GetBest.argtypes = ()
-        libicfp.GetBest.restype = c_char_p
 
     return libicfp
 
