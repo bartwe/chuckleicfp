2fdd7144d93b332635493fc4a3761692405b9306
catherine <catherine@debian.localdomain>
2012-07-15 22:37:53 -0500

    sdf

--- kyren/main.cpp
+++ kyren/main.cpp
@@ -11,10 +11,6 @@ static void sighandler(int signum) {
   auto solution = m_best.solution();
 	printf("# score: %d\n", solution.score);
 	printf("%s\n", commandString(solution.commands).c_str());
-  /*
-  Mine m(m_problem);
-  m.evaluateAndPrint(solution.commands);
-  */
 	abort();
 }
 
@@ -25,7 +21,7 @@ int main(int argc, char** argv) {
   srand(time(NULL));
 
   m_problem = Problem::read(std::cin);
-  RandomDijkstraSolver solver(m_best, 10000, 2000, 1000);
+  WeirdAStarSolver solver(m_best, 1000, 200, 100);
 
   while (true)
     solver.run(Mine(m_problem));
